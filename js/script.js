e570983336525f0e9b370b7ea0cb5287cd62f22e
E-McNeill <ericaksaunders@gmail.com>
2019-03-28 17:31:44 -0700

    add details for payment section

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 
-
 $('#name').focus(); //Makes the first text field in focus on page load. 
-
-$('#other-title').hide(); 
+$('#other-title').hide(); // Hides the job role box until it gets clicked
 
 //Shows and hides the job title box
 $('#title').change(function (){
@@ -22,8 +20,7 @@ $('#design').change(function (){
         $('#color option[value="cornflowerblue"]').show();
         $('#color option[value="darkslategrey"]').show();
         $('#color option[value="gold"]').show();
-    
-    
+       
     }   else if ($('#design').val() == 'heart js') {
             $('#color option[value="cornflowerblue"]').hide();
             $('#color option[value="darkslategrey"]').hide();
@@ -31,6 +28,7 @@ $('#design').change(function (){
             $('#color option[value="tomato"]').show();
             $('#color option[value="steelblue"]').show();
             $('#color option[value="dimgrey"]').show();
+    
         }  else if ($('#design').val() != 'heart js' && $('#design').val() != 'js puns'){
                 $('#color option[value="cornflowerblue"]').show();
                 $('#color option[value="darkslategrey"]').show();
@@ -41,27 +39,27 @@ $('#design').change(function (){
     }
 });
 
+//payment section
+//add class to other two payment divs for easy access. 
+var payPal = $('div.credit-card').next();
+payPal.addClass('PayPal');
+var bitCoin = $('div.PayPal').next();
+bitCoin.addClass('Bitcoin');
+// Paypal and bitcoin details hidden on load
+$('.PayPal').hide();
+$('.Bitcoin').hide();
 
+$('#payment').change(function (){
+    if ($('#payment').val() == 'paypal') { //If 'paypal' option is chosen, bitcoin and cc details removed.
+            $('.credit-card').hide();
+            $('.Bitcoin').hide();
+            $('.PayPal').show();
+      
+    } else if ($('#payment').val() == 'bitcoin') { // If 'bitcoin' is chosen, cc and paypal details removed.
+            $('.credit-card').hide();
+            $('.PayPal').hide();
+            $('.Bitcoin').show();
+    }  else $('.credit-card').show() && $('.Bitcoin').hide() && $('.PayPal').hide(); // When 'select payment method'is chosen, only CC info shows. 
+                 
 
-
-
-// label for="design">Design:</label>
-//           <select id="design" name="user_design">
-//             <option>Select Theme</option>
-//             <option value="js puns">Theme - JS Puns</option>
-//             <option value="heart js">Theme - I &#9829; JS</option>
-//           </select>
-//         </div>
-
-//         <div id="colors-js-puns" class="">
-//           <label for="color">Color:</label>
-//           <select id="color">
-//             <option value="cornflowerblue">Cornflower Blue (JS Puns shirt only)</option>
-//             <option value="darkslategrey">Dark Slate Grey (JS Puns shirt only)</option> 
-//             <option value="gold">Gold (JS Puns shirt only)</option> 
-//             <option value="tomato">Tomato (I &#9829; JS shirt only)</option>
-//             <option value="steelblue">Steel Blue (I &#9829; JS shirt only)</option> 
-//             <option value="dimgrey">Dim Grey (I &#9829; JS shirt only)</option> 
-//           </select>
-//         </div>                
-//       </fieldset>
+});
